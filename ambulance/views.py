fd78c518291bbf9f5502e4ce18c89188b342c934
John Mwanyika <87723962+JohnMwanyika@users.noreply.github.com>
2023-03-02 15:08:16 +0300

    including ambulance url

--- ambulance/views.py
+++ ambulance/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Hospital, Sub_counties, Ambulance
+from django.utils import timezone
 # Create your views here.
+def all_ambulance(request):
+    results = Ambulance.objects.all()
+    return render(request,'ambulance/home.html',{'ambulances':results})
